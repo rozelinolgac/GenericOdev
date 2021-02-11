4b631d2b7d07b4f8f6f9bd6158baa7e6a0dc4279
rozelin olğaç <rzlnolgac@gmail.com>
2021-02-11 16:56:22 +0300

    project c#

---
+++ Generic.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericOdev
+{
+    class Generic<TKey,TValue >
+    {
+        TKey[] Key;
+    TValue[] Value;
+    TKey[] TempKey;
+    TValue[] TempValue;
+
+    public Generic()
+    {
+        Key = new TKey[0];
+        Value = new TValue[0];
+    }
+
+
+    public void Add(TKey key, TValue value)
+    {
+        TempKey = Key;
+        TempValue = Value;
+
+        Key = new TKey[Key.Length + 1];
+        Value = new TValue[Value.Length + 1];
+
+        for (int i = 0; i < TempKey.Length; i++)
+            {
+            Key[i] = TempKey[i];
+            Value[i] = TempValue[i];
+        }
+
+        Key[Key.Length - 1] = key;
+        Value[Value.Length - 1] = value;
+
+    }
+    public void ToList()
+    {
+        for (int i = 0; i<Key.Length; i++)
+            {
+            Console.WriteLine(Key[i] +" -> "+Value[i]);
+        }
+    }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Generic<string, string> generic = new Generic<string, string>();
+            generic.Add("escalator", "yürüyen merdiven");
+            generic.Add("elevator", "asansör");
+            generic.ToList();
+        }
+    }
+}
